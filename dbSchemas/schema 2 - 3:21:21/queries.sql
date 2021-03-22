b1ce1443634625bbbc9010e3991aab647cb7bc0b
Marcus Lee <marcusslee95@berkeley.edu>
2021-03-21 22:33:22 -0700

    wrote 1st query to get all behaviors and partners of a specific user

--- dbSchemas/schema 2 - 3:21:21/queries.sql
+++ dbSchemas/schema 2 - 3:21:21/queries.sql
@@ -1,5 +1,5 @@
 
--- QUERY1: getting all behaviors and partners of a yser given user's id
+-- QUERY1: getting all behaviors and partners of a specific user
 
 -- -- Appraoch1: the worse approach - getting the partners and behaviors together in one query
 -- -- get all the 1 off behaviors and partners that monitor those behaviors for that user (downside is if multiple partners monitor one behavior.... that same behavior will be returned in join table multiple times.... I guess way to avoid this would be to divide it into multiple queries)
@@ -19,7 +19,7 @@ FROM one_off_behaviors_users_partners
 WHERE user_id = 1) as idOfPartnersOfOneOffBehaviorsOfUser
 JOIN partners ON partner_id = partners.id
 
--- pt2: gett all 1 off behaviors of user
+-- pt2: get all 1 off behaviors of user
 -- https://www.sqlbook.com/sql/sql-distinct-how-to-use/
 SELECT DISTINCT name, marker 
 FROM (SELECT one_off_behavior_id
@@ -27,13 +27,15 @@ FROM one_off_behaviors_users_partners
 WHERE user_id = 1) as idOfOneOffBehaviorsOfUser
 JOIN one_off_behaviors on one_off_behavior_id = one_off_behaviors.id
 
--- Pt3: do the same for repeated bhs 
+-- do the same for repeated bhs 
+-- Pt3: get all partners of user that monitor user's repeated behaviors
 SELECT relationship, email, report_frequency, status
 FROM (SELECT partner_id
 FROM repeated_behaviors_users_partners
 WHERE user_id = 1) as idOfPartnersOfRepeatedBehaviorsOfUser
 JOIN partners ON partner_id = partners.id
 
+-- pt4: get all repeated behaviors of user
 SELECT DISTINCT name, marker, frequency, amount
 FROM (SELECT repeated_behavior_id
 FROM repeated_behaviors_users_partners
